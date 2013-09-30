3f893935403d9707cf33ab3e7403ccfe036f5629
mike <mike@mike-ThinkPad-R400.(none)>
2013-09-30 14:56:33 -0400

    updated to dyndns address

--- android/rter/src/ca/nehil/rter/streamingapp2/LoginActivity.java
+++ android/rter/src/ca/nehil/rter/streamingapp2/LoginActivity.java
@@ -46,7 +46,8 @@ public class LoginActivity extends Activity {
 	private static final String[] DUMMY_CREDENTIALS = new String[] {
 			"anonymous", "anonymous" };
 	//private static final String SERVER_URL = "http://rter.cim.mcgill.ca";
-	private static final String SERVER_URL = "http://ec2-54-242-181-96.compute-1.amazonaws.com";
+	//private static final String SERVER_URL = "http://ec2-54-242-181-96.compute-1.amazonaws.com";
+	private static final String SERVER_URL = "http://rter.hopto.org";
 	//private static final String SERVER_URL = "http://142.157.36.21:8000";
 	/**
 	 * The default email to populate the email field with.
